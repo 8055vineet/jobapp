32132578929774de049f6f98f03e8fd695cdc9e1
patelvineet@8055 <patelvineet21u02020@gmail.com>
2024-10-03 14:23:00 +0530

    jobapp

---
+++ configserver/configserver/src/test/java/com/vineet/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vineet.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
